b5454d7a966b9592e86e2b1c3498f902232c4ee0
enapan <32618714+enapan@users.noreply.github.com>
2021-03-04 22:11:25 +0100

    Revert "continent version"
    This reverts commit 2d4c27a9423c6908e857e3a48434de9b8f8b5ddd.

--- index.js
+++ index.js
@@ -1,125 +1,87 @@
-
+const southWest = L.latLng(-62, -180),
+    northEast = L.latLng(84, 180),
+    bounds = L.latLngBounds(southWest, northEast);
+const map = L.map('map', {
+  maxBounds: bounds
+}).fitWorld();
 var popup;
 
-var myStyle = {
-  color: "cornflowerblue",
-  weight: 1,
-  opacity: 1,
-  fillOpacity: 0,
-  dashArray: '2, 5'
-};
-L.geoJSON(pacific).setStyle(myStyle).addTo(map);
-L.geoJSON(pacific2).setStyle(myStyle).addTo(map);
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png', {
+  minZoom: 2,
+  maxZoom: 18,
+  bounds: bounds,
+  attribution: '&copy;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy;<a href="https://cartodb.com/attributions">CartoDB</a>'
+}).addTo(map);
 
 const shuffledCountries = shuffle(countries);
 const state = {
-  target: null,
-  correct: 0,
-  wrong: 0,
-  skipped: 0
+  target: shuffledCountries.pop(),
+  tries: 0
 }
 
-function nextTarget() {
-  if (shuffledCountries.length > 0) {
-    state.target = shuffledCountries.pop();
-  } else {
-    alert(`Game over! Congratulations! \n You found all ${state.correct} countries, with ${state.wrong} incorrect guesses, and you skipped ${state.skipped} countries.`);
-  }
-  document.getElementById('target').textContent = normalizeCountryName(state.target.address.country);
-  updateUiState();
-}
-nextTarget();
-
-function updateUiState() {
-  document.getElementById('correct').textContent = state.correct;
-  document.getElementById('wrong').textContent = state.wrong;
-  document.getElementById('skipped').textContent = state.skipped;
-}
-
-document.getElementById('skip').onclick = function() {
-  state.skipped++;
-  nextTarget();
+function render() {
+  document.getElementById('target').textContent = showCountryName(state.target.address.country);
 }
 
 map.on('click', function(ev) {
   const latlng = ev.latlng;
-  const queryParams = { 'accept-language': 'en', format: 'json', lat: latlng.wrap().lat, lon: latlng.wrap().lng, zoom: 3 };
+  const queryParams = { format: 'json', lang: 'en', lat: latlng.lat, lon: latlng.lng, zoom: 3 };
   getJson('https://nominatim.openstreetmap.org/reverse', queryParams).then(function(place) {
-    if (place.error === "Unable to geocode") {
-      popup = L.popup().setLatLng(latlng).setContent("Nothing here").openOn(map);
-    } else if (place.error) {
-      console.error('reverse geocoding error: ' + place.error);
+    if (place.error) {
+      console.log('reverse geocoding error: ' + place.error);
     } else {
-      const countryName = normalizeCountryName(place.address.country || place.display_name);
-      const targetCountry = normalizeCountryName(state.target.address.country);
-      if (countryName === targetCountry) {
-        const popupText = `You correctly located <span class="correct">${targetCountry}</span>. Good job!`;
+      const countryName = place.address.country;
+      if (countryName === state.target.address.country) {
+        const correctLocation = showCountryName(countryName);
+        const popupText = `You correctly located <span class="correct">${correctLocation}</span>. Good job!`;
         popup = L.popup().setLatLng(latlng).setContent(popupText).openOn(map);
-        displayCountryShape(countryName, 'darkviolet').then(function() {
-          state.correct++;
-          nextTarget();
-        });
+        displayCountryShape(countryName, 'green');
+        randomize();
       } else {
-        const incorrectLocation = normalizeCountryName(countryName);
-        const distance = Math.round(L.latLng(state.target.lat, state.target.lon).distanceTo(latlng) / 1000)
-        const popupText = `You clicked on <span class="incorrect">${incorrectLocation}</span>, not ${targetCountry}. Try again!<br><span class="hint">Hint: ${targetCountry} is approximately ${distance} km away.</span>`;
+        const incorrectLocation = showCountryName(countryName || 'the sea');
+        const targetLocation = showCountryName(state.target.address.country);
+        const popupText = `You clicked on <span class="incorrect">${incorrectLocation}</span>, not ${targetLocation}. Try again!`;
         popup = L.popup().setLatLng(latlng).setContent(popupText).openOn(map);
-        displayCountryShape(countryName, 'orangered').then(function() {
-          state.wrong++;
-          updateUiState();
-        });
+        displayCountryShape(countryName, 'red');
       }
     }
   })
 });
 
-var wrongShape;
-
 function displayCountryShape(countryName, color) {
-  const queryParams = { 'accept-language': 'en', country: countryName, format: 'json', lang: 'en', polygon_geojson: 1, polygon_threshold: 0.01 };
-  return getJson('https://nominatim.openstreetmap.org/search', queryParams).then(function(places) {
-    const shapes = places.filter(function(place) {
-      return place.geojson.type.endsWith("Polygon") && place.display_name === countryName;
-    });
-    if (shapes[0]) {
+  const queryParams = { country: countryName, format: 'json', polygon_geojson: '1' };
+  getJson('https://nominatim.openstreetmap.org/search', queryParams).then(function(place) {
+    if (place[0]) {
       var geojson = {
-        type: 'Feature',
-        geometry: shapes[0].geojson
+        "type": "Feature",
+        "geometry": place[0].geojson
       };
-      if (wrongShape) wrongShape.removeFrom(map);
-      if (color === 'darkviolet') {
-        L.geoJSON(geojson).setStyle({ color: color, fillColor: color, weight: 1 }).addTo(map);
-      } else if (color === 'orangered') {
-        wrongShape = L.geoJSON(geojson).setStyle({ color: color, fillColor: color, weight: 1 });
-        wrongShape.addTo(map);  
-      }
+      L.geoJSON(geojson).setStyle({ color: color, fillColor: color, opacity: 0.7, weight: 1 }).addTo(map);
     } else {
-      console.error(`cannot display shape of country: ${countryName}`, places);
+      console.log('cannot display shape of country: ' + countryName);
     }
   });
 }
 
-function normalizeCountryName(countryName) {
+function showCountryName(countryName) {
   switch (countryName) {
-    //case "Czechia": return "Czech Republic";
-    //case "Democratic Republic of the Congo": return "Congo";
-    //case "Guam": return "United States of America";
+    case "Democratic Republic of the Congo": return "Congo";
     case "RDPA": return "Algeria";
     case "RSA": return "South Africa";
-    //case "Russian Federation": return "Russia";
-    //case "Sahrawi Arab Democratic Republic": return "Morocco";
-    case "SBA": return "Akrotiri and Dhekelia";
-    case "Sint Maarten": return "The Netherlands";
-    case "South Ossetia": return "Georgia";
-    //case "Territorial waters of Faroe Islands": return "Faroe Islands";
-    case "Turkish Republic Of Northern Cyprus": return "Cyprus";
-    //case "North Macedonia": return "Macedonia";
-    case "Mindanao": return "Philippines";
-    case "Luzon": return "Philippines";
-    case "Visayas": return "Philippines";
+    case "Russian Federation": return "Russia";
     default: return countryName;
   }
 }
+
+function randomize() {
+  if (shuffledCountries.length > 0) {
+    state.target = shuffledCountries.pop();
+  } else {
+    alert('Game over!');
+  }
+  render();
+}
+randomize();
 
 function shuffle(array) {
     let counter = array.length;
@@ -141,4 +103,11 @@ function buildUrl(baseUrl, queryParams) {
 
 function getJson(baseUrl, queryParams, callback) {
   return fetch(buildUrl(baseUrl, queryParams)).then(response => response.json());
+}
+
+function popupSnackbar(message) {
+  document.querySelector('#snackbar').MaterialSnackbar.showSnackbar({
+    message: message,
+    timeout: 3000
+  });
 }
